2f65e222e9758d7723e451bf6f4d7942f1960b3f
Naoki Yamaguchi <naoki@Naokis-MacBook-Pro.local>
2018-02-28 17:54:07 -0500

    adding config without api key

---
+++ config.js
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+  apiKey: "lsdaljflkdjfopnwefoaldfkjasldfj",
+  authDomain: "food-finder-c4fc4.firebaseapp.com",
+  databaseURL: "https://food-finder-c4fc4.firebaseio.com",
+  projectId: "food-finder-c4fc4",
+  storageBucket: "",
+  messagingSenderId: "29113191830"
+};
