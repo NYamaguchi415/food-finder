fc2ee399b9d8ae18f98f190a42b269e9ea0590f0
Naoki Yamaguchi <naoki@Naokis-MacBook-Pro.local>
2018-03-02 09:17:42 -0500

    added fake friendsList

--- screens/MainScreen.js
+++ screens/MainScreen.js
@@ -1,11 +1,34 @@
 import React, { Component } from 'react';
-import { View, Text } from 'react-native';
+import { View, Text, FlatList, Button } from 'react-native';
+import { List, ListItem } from 'react-native-elements';
 
 class MainScreen extends Component {
+  static navigationOptions = {
+    headerTitle: 'Food-Finder',
+    headerRight: (
+      <Button
+        onPress={() => alert('this is a button!')}
+        title='+'
+        color='blue'
+      />
+    )
+  }
+
   render() {
     return (
       <View>
-        <Text> OK TESTING </Text>
+        <Text>FRIENDS</Text>
+        <List>
+          <FlatList
+            data={[{ key: 'Friend1' }, { key: 'Friend2' }, { key: 'Friend3' }, { key: 'Friend4' }]}
+            renderItem={({ item }) =>
+              <ListItem
+                title={item.key}
+                hideChevron
+              />
+            }
+          />
+        </List>
       </View>
     );
   }
