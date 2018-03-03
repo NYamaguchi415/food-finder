0a42c86fb7b89c66c8eda369dd10a0b3a07dd5c8
Naoki Yamaguchi <naoki@Naokis-MacBook-Pro.local>
2018-03-03 15:09:35 -0500

    cleaning up some stuff

--- config.js
+++ config.js
@@ -1,8 +1,10 @@
 export const firebaseConfig = {
-  apiKey: "lsdaljflkdjfopnwefoaldfkjasldfj",
+  apiKey: "lasflajsfl;ajsdfljas",
   authDomain: "food-finder-c4fc4.firebaseapp.com",
   databaseURL: "https://food-finder-c4fc4.firebaseio.com",
   projectId: "food-finder-c4fc4",
   storageBucket: "",
   messagingSenderId: "29113191830"
 };
+
+export const yelpAPIKey = 'xx-x-x-ywnyx';
