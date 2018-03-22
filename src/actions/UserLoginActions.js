2f98092674d8b72549976d9472e84bba5bdc8080
Reyes, Roberto <roberto@havenlife.com>
2018-03-21 21:50:27 -0400

    convert usernameCheck into a promise for sign up

--- src/actions/UserLoginActions.js
+++ src/actions/UserLoginActions.js
@@ -45,29 +45,33 @@ export const loginUser = ({ email, password }) => {
 
 export const signupUser = ({ email, password, username }) => {
 	return (dispatch) => {
-		dispatch({ type: LOGIN_USER });
-		if (usernameCheck(username)) {
-			firebase.auth().createUserWithEmailAndPassword(email, password)
+		dispatch({ type: LOGIN_USER });	
+		usernameCheck(username).then((isAvailable)=>{
+			if (isAvailable) {
+				firebase.auth().createUserWithEmailAndPassword(email, password)
 				.then(user => createUserSuccess(dispatch, user, email, username))
-				.catch(() => loginUserFail(dispatch));
-		} else {
-			console.log('username taken');
-		}
+				.catch(() => loginUserFail(dispatch));				
+			} else {
+				console.log('taken');
+			}
+		})
 	};
 };
 
 const usernameCheck = (username) => {
-	let usernameGood = false;
-	firebase.database().ref('users')
-	.orderByChild('username').equalTo(username)
-	.on('value', snapshot => {
-		if (snapshot.exists()) {
-			usernameGood = false;
-		} else {
-			usernameGood = true;
-		}
-	});
-	return (usernameGood);
+	return new Promise((resolve, reject)=>{
+		let usernameGood = false;
+		firebase.database().ref('users')
+		.orderByChild('username').equalTo(username)
+		.on('value', snapshot => {
+			var val = snapshot.val();
+			if (val) {
+				return resolve(false);
+			} else {
+				return resolve(true);
+			}
+		})	
+	})
 };
 
 const writeUserData = (user, email, username) => {
